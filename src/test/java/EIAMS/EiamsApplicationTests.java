9e3e246a2d11ad372850d161cc30fd3572551b39
Tung Nguyen <tung.nguyen@netko-solution.com>
2024-01-14 20:33:10 +0700

    init

---
+++ src/test/java/EIAMS/EiamsApplicationTests.java
@@ -0,0 +1,13 @@
+package EIAMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
