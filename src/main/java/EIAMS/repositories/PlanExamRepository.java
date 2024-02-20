fd6480e637ac7164d3ffe9b29cf24f903b0bd6f7
tung208 <nguyentung9196@gmail.com>
2024-02-21 00:57:09 +0700

    modify entity with new database

---
+++ src/main/java/EIAMS/repositories/PlanExamRepository.java
@@ -0,0 +1,7 @@
+package EIAMS.repositories;
+
+import EIAMS.entities.PlanExam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanExamRepository extends JpaRepository<PlanExam, Integer> {
+}
