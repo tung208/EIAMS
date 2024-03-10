02322142a6c18041ec6be888855e99f5617e9472
tung208 <nguyentung9196@gmail.com>
2024-03-10 23:31:08 +0700

    api arrange student

--- src/main/java/EIAMS/repositories/RoomRepository.java
+++ src/main/java/EIAMS/repositories/RoomRepository.java
@@ -3,6 +3,9 @@ package EIAMS.repositories;
 import EIAMS.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Integer> {
     void deleteBySemesterId(int suid);
+    List<Room> findAllByType(String type);
 }
--- src/main/java/EIAMS/repositories/SchedulerRepository.java
+++ src/main/java/EIAMS/repositories/SchedulerRepository.java
@@ -3,5 +3,9 @@ package EIAMS.repositories;
 import EIAMS.entities.Scheduler;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
+    List<Scheduler> findAllBySemesterId(Integer semesterId);
+    Scheduler findBySemesterIdAndSlotIdAndRoomId(Integer semesterId, Integer slotId, Integer roomId);
 }
