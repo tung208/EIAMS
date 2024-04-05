d5cbe8571e88abc43dd2a9619324ef50c9f3dab9
tung208 <nguyentung9196@gmail.com>
2024-04-05 23:28:44 +0700

    fix api set exam code

--- src/main/java/EIAMS/repositories/SchedulerRepository.java
+++ src/main/java/EIAMS/repositories/SchedulerRepository.java
@@ -16,18 +16,22 @@ public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
     List<Scheduler> findAllBySemesterIdOrderByStartDate(Integer semesterId);
     List<Scheduler> findAllBySemesterIdAndSubjectCodeContainingOrderByStartDate(Integer semesterId, String subjectCode);
     Scheduler findBySemesterIdAndRoomIdAndStartDateAndEndDate(Integer semesterId, Integer roomId, LocalDateTime startDate, LocalDateTime endDate);
+
     @Query("""
             select s from Scheduler s
             where s.semesterId = ?1 and s.subjectCode <> ?2 and s.startDate = ?3 and s.endDate = ?4""")
     List<Scheduler> findAllBySemesterIdAndSubjectCodeNotAndStartDateAndEndDate(Integer semesterId, String subjectCode, LocalDateTime startDate, LocalDateTime endDate);
+
     @Query("""
             select s from Scheduler s
             where s.semesterId = ?1 and s.subjectCode not in ?2 and s.startDate = ?3 and s.endDate = ?4""")
     List<Scheduler> findAllBySemesterIdAndSubjectCodeNotInAndStartDateAndEndDate(Integer semesterId, Collection<String> subjectCode, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("select s from Scheduler s where s.semesterId = ?1 and s.startDate < ?2 and s.endDate > ?3 order by s.startDate asc")
+    @Query("select s from Scheduler s where s.semesterId = ?1 and ((s.startDate >= ?2 and s.endDate <= ?3) or (s.startDate <= ?2 and s.endDate >= ?2) or (s.startDate <= ?3 and s.endDate >= ?3)) order by s.startDate asc")
     List<Scheduler> findAllBySemesterIdAndStartDateBeforeAndEndDateAfter(Integer semesterId, LocalDateTime startDate, LocalDateTime endDate);
+
     void deleteBySemesterId(Integer semesterId);
+
     @Query("""
             select distinct s.subjectCode, s.startDate, s.endDate from Scheduler s
             where s.semesterId = ?1 and s.startDate > ?2 and s.endDate < ?3 and s.subjectCode like concat('%', ?4, '%')
@@ -35,19 +39,22 @@ public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
             """)
     List<Object> findAllBySemesterIdAndStartDateAfterAndEndDateBeforeAndSubjectCodeContains(
             Integer semesterId, LocalDateTime startDate, LocalDateTime endDate, String subjectCode);
+
     @Query("select distinct s.subjectCode, s.startDate, s.endDate from Scheduler s where s.semesterId = ?1 and s.subjectCode like concat('%', ?2, '%')" +
             "order by s.startDate asc")
     List<Object> findAllBySemesterIdAndSubjectCodeContains(Integer semesterId, String subjectCode);
+
     @Query("""
             select distinct s.subjectCode, s.startDate, s.endDate from Scheduler s
-            where s.semesterId = ?1 and s.startDate > ?2 and s.subjectCode like concat('%', ?3, '%')
+            where s.semesterId = ?1 and s.startDate >= ?2 and s.subjectCode like concat('%', ?3, '%')
             order by s.startDate asc
             """)
     List<Object> findAllBySemesterIdAndStartDateAfterAndSubjectCodeContains(
             Integer semesterId, LocalDateTime startDate, String subjectCode);
+
     @Query("""
             select distinct s.subjectCode, s.startDate, s.endDate from Scheduler s
-            where s.semesterId = ?1 and s.endDate < ?2 and s.subjectCode like concat('%', ?3, '%')
+            where s.semesterId = ?1 and s.endDate <= ?2 and s.subjectCode like concat('%', ?3, '%')
             order by s.startDate asc
             """)
     List<Object> findAllBySemesterIdAndEndDateBeforeAndSubjectCodeContains(
@@ -58,11 +65,12 @@ public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
     int countAllBySemesterIdAndLecturerId(Integer semesterId, Integer lecturerId);
 
     List<Scheduler> findAllBySemesterIdAndSubjectCodeIn(Integer semesterId, Collection<String> subjectCode);
+
     List<Scheduler> findAllBySemesterIdAndSubjectCodeNotIn(Integer semesterId, Collection<String> subjectCode);
 
     @Query("""
             select s from Scheduler s
-            where s.semesterId = ?1 and s.lecturerId = ?2 and ((s.startDate >= ?3 and s.endDate <= ?4) or (s.startDate > ?3 and s.endDate < ?4) or (s.startDate < ?3 and s.endDate > ?3) or (s.startDate < ?4 and s.endDate > ?4))""")
+            where s.semesterId = ?1 and s.lecturerId = ?2 and ((s.startDate >= ?3 and s.endDate <= ?4) or (s.startDate <= ?3 and s.endDate >= ?3) or (s.startDate <= ?4 and s.endDate >= ?4))""")
     List<Scheduler> findBySemesterIdAndLecturerIdAvailable(Integer semesterId, Integer lecturerId, LocalDateTime startDate, LocalDateTime endDate);
 
     @Modifying
--- src/main/java/EIAMS/services/interfaces/SchedulerServiceInterface.java
+++ src/main/java/EIAMS/services/interfaces/SchedulerServiceInterface.java
@@ -2,6 +2,7 @@ package EIAMS.services.interfaces;
 
 import EIAMS.entities.Scheduler;
 import EIAMS.entities.Student;
+import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface SchedulerServiceInterface {
     void arrangeStudent(int semesterId) throws Exception;
-    void setExamCode(int semesterId);
+    void setExamCode(int semesterId) throws Exception;
     void arrangeLecturer(int semesterId);
     List<List<String>> list(Integer semesterId, String search, String startDate, String endDate);
     Page<Student> getListStudentInARoom(Integer schedulerId,String search, Integer page, Integer limit);
