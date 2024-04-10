bf1813c26d2a9f91d15d214fb5156c2a74a855ce
tung208 <nguyentung9196@gmail.com>
2024-04-10 21:08:21 +0700

    order by date api index

--- src/main/java/EIAMS/repositories/SchedulerRepository.java
+++ src/main/java/EIAMS/repositories/SchedulerRepository.java
@@ -53,7 +53,6 @@ public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
     @Query("""
             select DISTINCT s.roomId, function('date', s.startDate) from Scheduler s
             where function('date', s.startDate) >= function('date', ?1) and function('date', s.endDate) <= function('date', ?2) and s.lecturerId = ?3
-            order by s.startDate asc
             """)
     List<Object[]> findAllRoomByDateAndLecturerId(LocalDateTime startDate, LocalDateTime endDate, Integer lecturerId);
 
@@ -68,7 +67,6 @@ public interface SchedulerRepository extends JpaRepository<Scheduler, Integer> {
     @Query("""
              select DISTINCT s.roomId, function('date', s.startDate) from Scheduler s
             where function('date', s.startDate) >= function('date', ?1) and function('date', s.endDate) <= function('date', ?2)
-            order by s.startDate asc
             """)
     List<Object[]> findAllRoomByDate(LocalDateTime startDate, LocalDateTime endDate);
 
