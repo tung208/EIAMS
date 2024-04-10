56b3067c1226c35ee2d42ae55f1f9f08bb03c72f
cityhunter2468 <levancuonglvt@gmail.com>
2024-04-10 20:42:11 +0700

    fix database

--- src/main/java/EIAMS/advice/ApplicationExceptionHandler.java
+++ src/main/java/EIAMS/advice/ApplicationExceptionHandler.java
@@ -1,11 +1,15 @@
 package EIAMS.advice;
 
+import EIAMS.entities.responeObject.ResponseObject;
 import EIAMS.exception.StudentException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,5 +31,16 @@ public class ApplicationExceptionHandler {
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("errorMessage", ex.getMessage());
         return errorMap;
+    }
+
+    /**
+     * Tất cả các Exception không được khai báo sẽ được xử lý tại đây
+     */
+    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ResponseObject> handleAllException(Exception ex, WebRequest request) {
+        // quá trình kiểm soat lỗi diễn ra ở đây
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ResponseObject("NOT OK", "Update Fail", ""));
     }
 }
