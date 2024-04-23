8e2330d2f2b2816583b6434e155d16be98f0c7ce
cityhunter2468 <levancuonglvt@gmail.com>
2024-04-23 17:27:16 +0700

    push database

--- src/main/java/EIAMS/controllers/PlanExamController.java
+++ src/main/java/EIAMS/controllers/PlanExamController.java
@@ -1,12 +1,15 @@
 package EIAMS.controllers;
 
 import EIAMS.dtos.PlanExamDto;
+import EIAMS.dtos.SchedulerSlotDto;
 import EIAMS.entities.PlanExam;
+import EIAMS.entities.Room;
 import EIAMS.entities.Scheduler;
 import EIAMS.entities.Semester;
 import EIAMS.entities.responeObject.PageResponse;
 import EIAMS.entities.responeObject.ResponseObject;
 import EIAMS.exception.EntityNotFoundException;
+import EIAMS.repositories.RoomRepository;
 import EIAMS.services.PlanExamService;
 import EIAMS.services.SchedulerService;
 import EIAMS.services.StatusService;
@@ -24,7 +27,9 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,6 +43,9 @@ public class PlanExamController {
 
     @Autowired
     SchedulerService schedulerService;
+
+    @Autowired
+    RoomRepository roomRepository;
 
     @PostMapping("/import")
     public ResponseEntity<ResponseObject> importPlanExam(@RequestParam("file") MultipartFile file,
@@ -114,7 +122,7 @@ public class PlanExamController {
     }
 
     @GetMapping("/get-list-slot")
-    public PageResponse<Scheduler> getListSlot(@RequestParam(defaultValue = "1") Integer pageNo,
+    public PageResponse<SchedulerSlotDto> getListSlot(@RequestParam(defaultValue = "1") Integer pageNo,
                                                @RequestParam(defaultValue = "2") Integer pageSize,
                                                @RequestParam(defaultValue = "id") String sortBy,
                                                @RequestParam(defaultValue = "") Integer semesterId,
@@ -122,7 +130,31 @@ public class PlanExamController {
                                                @RequestParam(defaultValue = "") String expectedTime
     ) throws ParseException {
         Page<Scheduler> page =  schedulerService.getListSlot(pageNo, pageSize, semesterId, expectedDate, expectedTime);
+
+//        prepare data
         List<Scheduler> schedulers = page.getContent();
-        return new PageResponse<>(page.getNumber() + 1, page.getTotalPages(), page.getSize(), page.getTotalElements(),schedulers);
+        List<Integer> roomId = schedulers.stream()
+                .map(obj -> ((Scheduler) obj).getRoomId())
+                .collect(Collectors.toList());
+        List<Room> rooms = roomRepository.findAllById(roomId);
+        Hashtable<Integer,String> roomHashtable = new Hashtable<>();
+        rooms.stream().forEach(element -> {
+            roomHashtable.put(element.getId(), element.getName());
+        });
+        List<SchedulerSlotDto> schedulerSlotDtos = new ArrayList<>();
+
+        schedulers.stream().forEach(element -> {
+            SchedulerSlotDto schedulerSlotDto = SchedulerSlotDto.builder()
+                    .id(element.getId())
+                    .semesterId(element.getSemesterId())
+                    .roomId(element.getRoomId())
+                    .roomName(roomHashtable.get(element.getRoomId()))
+                    .startDate(element.getStartDate())
+                    .endDate(element.getEndDate())
+                    .type(element.getType())
+                    .build();
+            schedulerSlotDtos.add(schedulerSlotDto);
+        });
+        return new PageResponse<>(page.getNumber() + 1, page.getTotalPages(), page.getSize(), page.getTotalElements(),schedulerSlotDtos);
     }
 }
